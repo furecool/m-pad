074151e2bfe1ca6a5095a24a21927675b196fba4
furecool <furecool@gmail.com>
2022-05-10 11:02:33 +0800

    update for gh-pages setting

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+    chainWebpack: config => {
+        config.plugin('html')
+            .tap(args => {
+            args[0].title = "台新銀行──營業廳經理平板"
+            return args;
+            })
+    },
     publicPath: './',
     // outputDir: 'dist',
     // assetsDir: './',
